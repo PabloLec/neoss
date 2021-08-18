1591fba6fb4082ba3e7cdf65a688ea9c6b086777
PabloLec <pablo.lecolinet@gmail.com>
2021-08-18 11:23:50 +0200

    Move ss functions in a new module

--- index.js
+++ index.js
@@ -1,95 +1,8 @@
-var whoisJson = require("whois");
-
 var blessed = require("neo-blessed");
 var Table = require("./table");
 const pino = require("pino");
 const logger = pino(pino.destination("/tmp/node.log"));
-
-const { spawn } = require("child_process");
-
-const ls = spawn("ss", ["-OHrtupn"]);
-var os = require("os");
-var whoisJson = require("whois");
-
-var cmdOutput = "";
-var connections = [];
-
-ls.stdout.on("data", (data) => {
-  cmdOutput += data;
-});
-
-ls.stderr.on("data", (data) => {
-  console.log(`stderr: ${data}`);
-});
-
-ls.on("error", (error) => {
-  console.log(`error: ${error.message}`);
-});
-
-ls.on("close", (code) => {
-  //console.log(`child process exited with code ${code}`);
-  formatOutput(cmdOutput);
-  table.setData({
-    headers: [
-      "protocol",
-      "state",
-      "receiveQueue",
-      "sendQueue",
-      "localAddress",
-      "localPort",
-      "peerAddress",
-      "peerPort",
-      "users",
-    ],
-    data: connections,
-    colWidth: [5, 5, 5, 5, 20, 10, 20, 10, 10, 10],
-  });
-  screen.render();
-});
-
-function formatOutput(data) {
-  let outputLines = data.split(os.EOL);
-  for (let i = 0; i < outputLines.length; i++) {
-    if (outputLines[i].includes("users")) {
-      line = outputLines[i]
-        .split("users:")[0]
-        .split(" ")
-        .filter((n) => n);
-
-      users = outputLines[i].split("users:")[1];
-    } else {
-      line = outputLines[i].split(" ").filter((n) => n);
-      users = null;
-    }
-    if (line.length == 0) {
-      continue;
-    }
-
-    connections[i] = {};
-    connections[i].protocol = line[0];
-    connections[i].state = line[1];
-    connections[i].receiveQueue = line[2];
-    connections[i].send = line[3];
-    connections[i].localAddress = line[4].match("([^:]+):")[1];
-    connections[i].localPort = line[4].match(":([0-9]+)")[1];
-    connections[i].peerAddress = line[5].match("([^:]+):")[1];
-
-    connections[i].peerPort = line[5].match(":([0-9]+)")[1];
-
-    if (users) {
-      usersList = users.match(/(\"[^\"]+\"\,pid=[0-9]+\,fd=[0-9]+)/g);
-      connections[i].users = {};
-      for (let j = 0; j < usersList.length; j++) {
-        connections[i].users[j] = {};
-        connections[i].users[j].name = usersList[j].match('"([^"]+)"')[1];
-        connections[i].users[j].pid = usersList[j].match("pid=([0-9]+)")[1];
-      }
-    } else {
-      connections[i].users = null;
-    }
-    connections[i].users = "test";
-  }
-}
+const ss = require("./ssWrapper");
 
 // Create a screen object.
 var screen = blessed.screen({
@@ -101,7 +14,6 @@ screen.title = "my window title";
 var table = Table({
   keys: true,
   interactive: true,
-  scrollable: true,
   tags: true,
   top: "0",
   left: "center",
@@ -122,6 +34,8 @@ var table = Table({
     },
   },
 });
+
+ss.ss(screen, table);
 
 // Append our box to the screen.
 screen.append(table);
